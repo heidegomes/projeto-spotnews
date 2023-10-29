56d8cbdbf7b527c94212744674319f0cd2c407b8
heidegomes <heidegc@hotmail.com>
2023-10-29 13:37:49 -0300

    Requisito 5 em andamento!

--- news/views.py
+++ news/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from news.models import News
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
     context = {"title": "Spot News", "news": News.objects.all()}
     return render(request, "home.html", context)
+
+
+def new_details(request, id):
+    new = get_object_or_404(News, id=id)
+    return render(request, "news_details.html", {"new": new})
