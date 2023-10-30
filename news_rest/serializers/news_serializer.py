f8124cf39b68226641761f1288fbd381a4517a82
heidegomes <heidegc@hotmail.com>
2023-10-30 19:06:18 -0300

    Requisito 10 feito!

--- news_rest/serializers/news_serializer.py
+++ news_rest/serializers/news_serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from news.models import News
 
 
-class NewsSerializer(serializers.HyperlinkedModelSerializer):
+class NewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = News
         fields = [
